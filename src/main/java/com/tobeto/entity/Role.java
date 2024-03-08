967f2561ce26149d44366dbadc8988d66b596015
Levent GUREN <levent.guren@gmail.com>
2024-03-08 19:51:50 +0300

    guncelleme

---
+++ src/main/java/com/tobeto/entity/Role.java
@@ -0,0 +1,30 @@
+package com.tobeto.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQuery;
+import lombok.Data;
+
+/**
+ * The persistent class for the role database table.
+ * 
+ */
+@Entity
+@NamedQuery(name = "Role.findAll", query = "SELECT r FROM Role r")
+@Data
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "role_name")
+	private String roleName;
+	@ManyToOne
+	private User user;
+}
